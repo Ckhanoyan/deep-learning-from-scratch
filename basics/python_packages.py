18c24f9b057e5e6877f0dd44f57fe65b608e1c06
Chris Khanoyan <157422273+Ckhanoyan@users.noreply.github.com>
2024-12-07 05:39:31 -0500

    Create python_packages.py

---
+++ basics/python_packages.py
@@ -0,0 +1,3 @@
+## The statement import numpy as np is a Python command that imports the NumPy library, a powerful package for numerical computing, and gives it the alias np for convenience. This allows you to use NumPy functions and objects by prefixing them with np, such as np.array() for creating arrays.
+
+import numpy as np 
