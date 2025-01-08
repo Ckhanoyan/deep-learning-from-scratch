c470d2bfdd719edbc7eb9cc5a031d83e5c674675
Chris Khanoyan <157422273+Ckhanoyan@users.noreply.github.com>
2025-01-08 06:09:56 -0500

    Create dropout.py
    Added this example

---
+++ scripts/regularization/dropout.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
